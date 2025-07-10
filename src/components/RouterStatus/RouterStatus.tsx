1dec58764c6d7803cfa187c264d0b793e09daf80
X-O1 <series-X-01@protonmail.com>
2025-07-10 03:31:25 -0700

    Updated

--- src/components/RouterStatus/RouterStatus.tsx
+++ src/components/RouterStatus/RouterStatus.tsx
@@ -4,8 +4,12 @@ const RouterStatus = () => {
     <>
       <div className={styles.container}>
         <span className={styles.title}>Router Status</span>
+        <span className={styles.subTitle}>Nickname: Genesis</span>
+        <span className={styles.subTitle}>Currency: aUSDC(0xabc...123)</span>
+        <span className={styles.subTitle}>Balance: 1000</span>
         <span className={styles.subTitle}>Active: true</span>
         <span className={styles.subTitle}>Destination: 0xabc...123</span>
+        <span className={styles.subTitle}>Automation Fee: 0.5% (auto-deducted)</span>
       </div>
     </>
   );
